d48a5ff8e84b10131b771b012abbd06452b1c632
Dalelit <dale.coding@gmail.com>
2018-03-28 22:28:48 +1100

    More stuff

--- learnPython.py
+++ learnPython.py
@@ -1,9 +1,36 @@
 import threading
 
+
+class aSimpleClass:
+
+    ''' A simple class to play with classes.
+    '''
+
+    classMethodCounter = 0
+    classInstanceCounter = 0
+
+    def __init__(self, someInfo):
+        aSimpleClass.classInstanceCounter += 1
+        self.someInfo = someInfo
+
+    def aMethod(self):
+        print("Doing a simple method {} {}".format(self.classInstanceCounter, self.someInfo))
+
+    @classmethod
+    def aClassMethod(cls):
+        cls.classMethodCounter += 1
+        print("Class method {}".format(cls.classMethodCounter))
+
+    @staticmethod
+    def aStaticMethod():
+        print("Doing a statis method... woo hoo")
+
+
+
 class doStuff(threading.Thread):
 
     def run(self):
-        learn_sets_and_dictionaries()
+        learn_sets()
 
 def my_first_decorator(func):
     def wrap():
@@ -25,13 +52,40 @@ def learn_lists():
     l4 = list(map(lambda x: x+1, l3))
     print(l4)
 
-def learn_sets_and_dictionaries():
+    myList = ['a', 'b', 'c', 'g', 'f']
+
+    print(myList)
+    myList.sort()
+    print(myList)
+
+    myList2 = ['x','y','z']
+
+    myList.extend(myList2)
+    print(myList)
+
+    # for thing in myList:
+    #     print(thing)
+
+    for indx, thing in enumerate(myList):
+        print(indx, thing)
+
+    print(', '.join(myList))
+
+    print('b' in myList)
+
+def learn_sets():
     s1 = {1,2,3,4,5,6,7}
     print(s1)
     s1.add(4)
     s1.add(8)
     print(s1)
 
+    s2 = {4,8,16,32}
+    print(s1.intersection(s2))
+    print(s1.difference(s2))
+    print(s1.union(s2))
+    
+def learn_dictionaries():
     d1 = dict(dale=1, dana=2, lynn=3, abby=4)
     print(d1)
     d2 = {'a':3, 'b':7, 'c':4}
@@ -50,19 +104,31 @@ def learn_sets_and_dictionaries():
     for i,k in enumerate(d2):
         print(("index {}, value {}").format(i,v))
 
-def abbys_first_function():
-    print("Hello world. Abby and Dale are the best!")
+
+def play_with_strings():
+    lst = ['something', 222]
+    print(f"{lst[0]} then {lst[1]}")
 
 if __name__ == '__main__':
-    # learn_sets_and_dictionaries()
+
+    # learn_dictionaries()
+    # learn_sets()
     # learn_lists()
+    play_with_strings()
 
     # stuff = doStuff()
     # stuff.start()
     # print("Main thread doing stuff")
     # stuff.join()
 
-    for i in range(0,8):
-        abbys_first_function()
+
+    # aCls = aSimpleClass("Blah Blah")
+    # aCls.aMethod()
+    # aCls.aClassMethod()
+    # aSimpleClass.aClassMethod()
+    # aCls.aClassMethod()
+    # aCls.aStaticMethod()
+    # print(aSimpleClass.__dict__)
+    # print(aCls.__dict__)
 
     print("Done!")
